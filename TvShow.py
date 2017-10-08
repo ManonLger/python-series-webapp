0a5b66689c5db3f0a8d22ca5dc560f2f98312db0
Alexandre Sioufi <sioufia78@gmail.com>
2017-10-08 23:30:16 +0200

    Structured the class TV show

--- TvShow.py
+++ TvShow.py
@@ -1,17 +1,19 @@
-serie = json.loads(r)
-print (serie)
+import requests
+import json
 
-nom_serie = serie["name"] # Pour récupérer le nom de la série
-print (nom_serie)
+class TvShow:
+    """Definition of the class TvShow, it contains the following attributes :
+       - id of the tvshow
+       - name
+       - overview : the description of the tvshow
+       - nb_season : number of seasons in the tvshow
+       - in_production : if set to True, the tvshow is still in production ; if set to False, it's been over"""
+    def __init__(self,id):
+        url = "https://api.themoviedb.org/3/tv/" + str(self.id)
+        url_content = json.loads(requests.get(url, params={"api_key":"3b8efb3a7bff18de4e8641510c6352a5"}).content.decode())
+        self.id = id
+        self.name = url_content["name"]
+        self.overview = url_content["overview"]
+        self.nb_season = url_content["number_of_seasons"]
+        self.in_production = url_content["in_production"]
 
-resume = serie["overview"] # Pour récupérer le résumé d'une série
-print (resume)
-
-nb_saison = serie["number_of_seasons"] # Pour récupérer le nombre de saisons dans la série
-print(nb_saison)
-
-en_production = serie["in_production"] # savoir si la série est toujours en production
-if en_production:
-    print("En cours")
-else:
-    print("Plus en cours")
