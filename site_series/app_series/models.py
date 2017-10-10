461939c79299ffd8d26844666e884fbd5699162b
depreza <32543068+depreza@users.noreply.github.com>
2017-10-10 15:52:13 +0200

    Modify Season class

--- site_series/app_series/models.py
+++ site_series/app_series/models.py
@@ -32,7 +32,59 @@ class TvShow(models.Model):
             self.in_production = url_content["in_production"]
 
 class Season(models.Model):
-    num = models.IntegerField()
+    """Definition of the class TvShowSeason, it contains the following attributes:
+            - id of the TvShow
+            - season_nb: season number
+            - id of the season
+            - name : season name
+            - overview : the description of the season
+            - broadcast_date : the release date of the season
+            - a list of episodes within the season
+            """
+
+    tv_id = models.IntegerField()
+    season_nb = models.IntegerField()
+    season_id = models.IntegerField()
+    name = models.CharField()
+    overview = models.CharField()
+    broadcast_date = models.DateTimeField(verbose_name="Release Date")
+
+    def __str__(self):
+        return self.tv_id
+    def __str__(self):
+        return self.season_nb
+
+    def find_name(self):
+
+
+
 
 class Episode(models.Model):
     num = models.IntegerField()
+
+import requests
+import json
+
+class TvShowSeason:
+
+        def __init__(self, tv_id=int, season_nb=int):
+            url = "https://api.themoviedb.org/3/tv/" + str(self.tv_id) + "season/" + str(self.season_nb)
+            url_content = json.loads(requests.get(url, params={"api_key": "3b8efb3a7bff18de4e8641510c6352a5"}).content.decode())
+            self.tv_id = tv_id
+            self.season_nb = season_nb
+            self.id_season = url_content["id"]
+            self.name = url_content["name"]
+            self.overview = url_content["overview"]
+            self.season_nb = url_content["season_number"]
+            self.broadcast_date = url_content["air_date"]
+
+            """Definition of the function returning a list of the episodes within the season"""
+            def get_episodes_list(self):
+                list_episodes=[]
+                episodes= url_content["episodes"]
+                for elt in episodes:
+                    episode_nb =  episodes["episodes_number"]
+                    episode_name = episodes["name"]
+                    episode_overview = episodes["overview"]
+                    l.append(episode_nb, episode_name, episode_overview)
+                return list_episodes
