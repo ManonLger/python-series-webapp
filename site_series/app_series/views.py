f5acda697e2c6082d89517cdaf42c6128eafc622
Manon <manon.leger@ntymail.com>
2017-10-11 19:41:46 +0200

    Made a better view thanks to a template

--- site_series/app_series/views.py
+++ site_series/app_series/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from .models import TvShow
+import requests
+import json
+
+base_url = "https://api.themoviedb.org/3/"
 
 def index(request):
-    list = ", ".join([str(tvs) for tvs in TvShow.objects.all()])
-    return HttpResponse("Liste de séries : "+list)
+    params = {
+        "sort_by": "popularity.desc",
+        "api_key": "XXX"
+    }
+    r = requests.get(base_url+"discover/tv", params=params).content.decode()
+    r = json.loads(r)["results"]
+    list = []
+    for tvs in r:
+        list += [TvShow(title = tvs["name"])]
+
+    context = {
+        "list": list
+    }
+    return render(request, 'app_series/index.html', context)
