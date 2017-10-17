36c7c5b5f9da72924eb59da970192f74dfd1b975
Manon <manon.leger@ntymail.com>
2017-10-17 15:22:19 +0200

    Set up some environment settings

--- site_series/app_series/views.py
+++ site_series/app_series/views.py
@@ -3,19 +3,20 @@ from django.http import HttpResponse
 from .models import TvShow
 import requests
 import json
+import site_series.settings as settings
 
 base_url = "https://api.themoviedb.org/3/"
 
 def index(request):
     params = {
         "sort_by": "popularity.desc",
-        "api_key": "XXX"
+        "api_key": settings.TMDB_API_KEY
     }
     r = requests.get(base_url+"discover/tv", params=params).content.decode()
     r = json.loads(r)["results"]
     list = []
     for tvs in r:
-        list += [TvShow(title = tvs["name"])]
+        list += [TvShow(title = tvs["name"], id = tvs[])]
 
     context = {
         "list": list
