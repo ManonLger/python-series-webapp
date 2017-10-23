30064ce4fff1aabbce942a7ac40cba29f58d0d67
Alexandre Sioufi <sioufia78@gmail.com>
2017-10-23 18:55:37 +0200

    Corrected a bug in displayed of list of seasons and added the next episode date in the series and wishlist templates

--- site_series/app_series/views.py
+++ site_series/app_series/views.py
@@ -25,7 +25,8 @@ def index(request):
 def view_serie(request,id):
     serie = TvShow()
     serie.set_series_attributes(id)
-    liste_saison = range(1, serie.nb_season)
+    liste_saison = range(1, serie.nb_season+1)
+    next_episode = serie.get_next_episode_run_time()
     return render(request, 'app_series/serie.html',locals())
 
 def view_season(request, tv_show, season_nb):
