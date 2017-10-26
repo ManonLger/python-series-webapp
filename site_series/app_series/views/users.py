88391d82e49e829985aaa8b0bd4e8910f8da39cc
Alexandre Sioufi <sioufia78@gmail.com>
2017-10-26 16:29:04 +0200

    Added the link between a wishlist and an user

--- site_series/app_series/views/users.py
+++ site_series/app_series/views/users.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render
 
-from app_series.models import Wishlist
+from app_series.models import Wishlist, TvShow
+from django.views.generic import ListView
+
 
 def signup(request):
     error = False
@@ -11,7 +13,7 @@ def signup(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user.wishlist = Wishlist(user=user)
+            Wishlist.objects.create(user=user) #A chaque fois qu'un utilisateur est créée, on lui associe une wishlist vide
             go_to_login = True
         else:
             error = True
@@ -19,5 +21,13 @@ def signup(request):
         form = UserCreationForm()
 
     return render(request, 'registration/signup.html', locals())
+
+class WishListView(ListView):
+    model = TvShow
+    template_name = "app_series/wishlist.html"
+
+    def get_queryset(self):
+        user = self.request.user #On récupère l'utilisateur connecté
+        return user.wishlist.wishes.all() #On renvoie la liste des wishes de cet utilisateur
 
 
